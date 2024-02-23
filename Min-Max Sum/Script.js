ae68048f0b908e68fdc735770f5e58f83e7c5598
Gustavo Thyerris <132522484+GusThyerris@users.noreply.github.com>
2024-02-22 21:49:21 -0300

    Add files via upload

---
+++ Min-Max Sum/Script.js
@@ -0,0 +1,55 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+    // Write your code here
+    let minSum = 0, maxSum = 0;
+    let min = arr[0], max = arr[0];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    let sum = arr.reduce((acc, curr) => acc + curr, 0);
+
+    minSum = sum - max;
+    maxSum = sum - min;
+
+    console.log(minSum, maxSum);
+}
+
+function main() {
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
